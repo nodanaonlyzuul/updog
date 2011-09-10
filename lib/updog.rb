f81cc3d6b740c063134ff0e28f7b28baeda9a8ab
Stephen Tiberius Schor <beholdthepanda@gmail.com>
2011-09-09 22:25:44 -0400

    Gemmmmiiffyyying

---
+++ lib/updog.rb
@@ -0,0 +1,26 @@
+require "rubygems"
+require "nokogiri"
+require 'open-uri'
+
+module Updog  
+  def self.get_weather(area)
+    url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query="
+    document  = Nokogiri::XML(open(url << area))
+    
+    qualitive = document.css('fcttext')
+    
+    document.css("simpleforecast forecastday").each_with_index do |forecastday, i|
+      highs = forecastday.css('high')
+      lows  = forecastday.css('low')
+      
+      print "\n"
+      print forecastday.css("date weekday").first.content
+      print "\n"
+      print " High: #{highs.css('fahrenheit').first.content} F / #{highs.css('celsius').first.content} C \n"
+      print " Low:  #{lows.css('fahrenheit').first.content} F / #{lows.css('celsius').first.content} C   \n"
+      print " #{qualitive[i].content} \n" if qualitive[i]
+    end
+    
+    print "\n"
+  end
+end
---
+++ updog.gemspec
@@ -0,0 +1,16 @@
+$spec = Gem::Specification.new do |s|
+  s.name = "updog"
+  s.version = '0.0.1'
+  s.summary = "A Command-Line tool for getting the weather"
+
+  s.authors  = ['Stephen Schor']
+  s.email    = ['beholdthepanda@gmail.com']
+  s.homepage = 'https://github.com/nodanaonlyzuul/updog'
+
+  s.executables = ['updog']
+
+  s.files = Dir['bin/*','lib/**/*']
+  s.rubyforge_project = 'nowarning'
+  
+  s.add_dependency('nokogiri', '1.5.0')
+end
